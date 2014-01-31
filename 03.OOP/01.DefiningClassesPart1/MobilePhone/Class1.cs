f66b4b61202caa7df97dfa882670af88faf58fa6
neutrino <jrusev@gmail.com>
2014-01-31 14:59:58 +0200

    added file headers and summary

--- 03.OOP/01.DefiningClassesPart1/MobilePhone/Class1.cs
+++ 03.OOP/01.DefiningClassesPart1/MobilePhone/Class1.cs
@@ -1,11 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Class1.cs" company="Sprocket Enterprises">
+//     Copyright (c) Sprocket Enterprises. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace MobilePhone
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Mobile phone device
+    /// </summary>
     public class Class1
     {
     }
