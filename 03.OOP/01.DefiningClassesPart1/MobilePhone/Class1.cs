e3e5b7cbe16d28962ae90a87582137e5fbdf27b5
neutrino <jrusev@gmail.com>
2014-01-31 15:07:49 +0200

    added task description

--- 03.OOP/01.DefiningClassesPart1/MobilePhone/Class1.cs
+++ 03.OOP/01.DefiningClassesPart1/MobilePhone/Class1.cs
@@ -4,6 +4,13 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
+/*
+ * Define a class that holds information about a mobile phone device:
+ * model, manufacturer, price, owner, battery characteristics (model, hours idle and hours talk)
+ * and display characteristics (size and number of colors).
+ * Define 3 separate classes (class GSM holding instances of the classes Battery and Display).
+ */
+
 namespace MobilePhone
 {
     using System;
