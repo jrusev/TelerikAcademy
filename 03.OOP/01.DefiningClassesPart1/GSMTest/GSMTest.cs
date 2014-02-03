5ccebb45de686a8ded132c4eca6fa0c63179f5e0
neutrino <jrusev@gmail.com>
2014-02-03 21:16:24 +0200

    Tested static var iPhone4S

--- 03.OOP/01.DefiningClassesPart1/GSMTest/GSMTest.cs
+++ 03.OOP/01.DefiningClassesPart1/GSMTest/GSMTest.cs
@@ -12,8 +12,8 @@ public class GSMTest
         // Create an array of few instances of the GSM class.
         GSM[] testPhones = new GSM[3];
         testPhones[0] = new GSM("Galaxy 4", "Samsung");
-        testPhones[1] = new GSM("iPhone 5S", "Apple", new Battery("1440 mAh", 10, 3, BatteryType.LiIo), new Display(5, 16000000));
-        testPhones[2] = new GSM("iPhone 5C", "Apple", 500, "John Dow", new Battery("1440 mAh", 10, 3, BatteryType.NiMH), new Display(5, 16000000));
+        testPhones[1] = new GSM("iPhone 5C", "Apple", 350, "John Dow", new Battery("1440 mAh", 10, 3, BatteryType.NiMH), new Display(5, 16000000));
+        testPhones[2] = GSM.IPhone4S; // Testing the static variable
 
         // Display the information about the GSMs in the array.
         foreach (var phone in testPhones)
