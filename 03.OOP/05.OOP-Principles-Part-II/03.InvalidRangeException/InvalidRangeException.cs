d01b1cd705c03a8fa67c81f036c21463d873503d
neutrino <jrusev@gmail.com>
2014-02-14 23:14:15 +0200

    completed all tasks (01-03)

---
+++ 03.OOP/05.OOP-Principles-Part-II/03.InvalidRangeException/InvalidRangeException.cs
@@ -0,0 +1,15 @@
+﻿public class InvalidRangeException<T> : System.ApplicationException    
+{
+    private const string OutOfRange = "Value out of range ({0} - {1}).";
+
+    public InvalidRangeException(T min, T max)
+        : base(string.Format(OutOfRange, min, max))
+    {
+        this.Min = min;
+        this.Max = max;
+    }
+
+    public T Min { get; private set; }
+
+    public T Max { get; private set; }
+}
---
+++ 03.OOP/05.OOP-Principles-Part-II/03.InvalidRangeException/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+internal class Program
+{
+    // Define a class InvalidRangeException<T> that holds information about an error condition related to invalid range.
+    // It should hold error message and a range definition [start … end].
+    // Write a sample application that demonstrates the InvalidRangeException
+    // by entering numbers in the range [1..100] and dates in the range [1.1.1980 … 31.12.2013].
+    private static void Main()
+    {
+        try
+        {
+            int min = 1;
+            int max = 100;
+            int number = int.MinValue;
+            if (number < min || number > max)
+            {
+                throw new InvalidRangeException<int>(min, max);
+            }
+        }
+        catch (InvalidRangeException<int> ex)
+        {
+            Console.WriteLine("Exception caught: {0}", ex.Message);
+        }
+
+        try
+        {
+            DateTime minDate = new DateTime(1980, 1, 1);
+            DateTime maxDate = new DateTime(2013, 12, 31);
+            DateTime date = DateTime.MinValue;
+            if (date < minDate || date > maxDate)
+            {
+                throw new InvalidRangeException<DateTime>(minDate, maxDate);
+            }
+        }
+        catch (InvalidRangeException<DateTime> ex)
+        {
+            Console.WriteLine("Exception caught: {0}", ex.Message);
+        }
+    }
+}
