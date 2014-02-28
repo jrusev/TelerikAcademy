f4f34b9b461fb76c66e41826418d615cc1cc8432
neutrino <jrusev@gmail.com>
2014-02-28 15:54:07 +0200

    Task5 solved

---
+++ 03.OOP/06.CommonTypeSystem/05.BitArray64/BitArray64.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+// Define a class BitArray64 to hold 64 bit values inside an ulong value.
+// Implement IEnumerable<int> and Equals(…), GetHashCode(), [], == and !=.
+public class BitArray64 : IEnumerable<int>
+{
+    private const byte Size = 64;
+    private ulong bits;
+
+    public BitArray64(ulong bits)
+    {
+        this.bits = bits;
+    }
+
+    /// <summary>
+    /// Gets the value of the bit at a specific position in the BitArray64.
+    /// </summary>
+    /// <param name="index">The zero-based index of the value to get.</param>
+    /// <returns>The int value of the bit at position index.</returns>
+    public int this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= BitArray64.Size)
+            {
+                throw new ApplicationException("Index is outside the range of the BitArray64!");
+            }
+
+            return (int)((this.bits >> index) & 1);
+        }
+    }
+
+    public static bool operator ==(BitArray64 ba1, BitArray64 ba2)
+    {
+        return BitArray64.Equals(ba1, ba2);
+    }
+
+    public static bool operator !=(BitArray64 ba1, BitArray64 ba2)
+    {
+        return !BitArray64.Equals(ba1, ba2);   
+    }
+
+    public override bool Equals(object obj)
+    {
+        BitArray64 ba = obj as BitArray64;
+        if (ba == null)
+        {
+            return false;
+        }
+
+        return this.bits == ba.bits;
+    }
+
+    public override int GetHashCode()
+    {
+        return this.bits.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder(64);
+        foreach (var bit in this)
+        {
+            sb.Append(bit);
+        }
+
+        return sb.ToString();
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        for (int i = 0; i < BitArray64.Size; i++)
+        {
+            yield return this[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        for (int i = 0; i < BitArray64.Size; i++)
+        {
+            yield return this[i];
+        }
+    }
+}
---
+++ 03.OOP/06.CommonTypeSystem/05.BitArray64/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+internal class Program
+{
+    // Define a class BitArray64 to hold 64 bit values inside an ulong value.
+    // Implement IEnumerable<int> and Equals(…), GetHashCode(), [], == and !=.
+    internal static void Main()
+    {
+        // Let's use int.MaxValue (32 one's) to set the BitArray64
+        BitArray64 bitArray1 = new BitArray64(int.MaxValue);
+
+        // Use IEnumerable to foreach the elements in the BitArray64
+        Console.Write("bitArray1: ");
+        foreach (var bit in bitArray1)
+        {
+            Console.Write(bit);
+        }
+
+        // Use BitArray64.ToString to print the second bitarray
+        BitArray64 bitArray2 = new BitArray64(int.MaxValue - 1);
+        Console.WriteLine("\nbitArray2: {0}", bitArray2);
+
+        // Test BitArray64.Equals, ==, !=
+        Console.WriteLine();
+        Console.WriteLine("bitArray1.Equals(bitArray2) -> {0}", bitArray1.Equals(bitArray2));
+        Console.WriteLine("(bitArray1 == bitArray2) -> {0}", bitArray1 == bitArray2);
+        Console.WriteLine("(bitArray1 != bitArray2) -> {0}", bitArray1 != bitArray2);
+
+        // Test BitArray64.GetHashCode
+        Console.WriteLine();
+        Console.WriteLine("bitArray1 HashCode: {0}", bitArray1.GetHashCode());
+        Console.WriteLine("bitArray2 HashCode: {0}", bitArray2.GetHashCode());
+
+        // Test indexers
+        Console.WriteLine();
+        Console.WriteLine("bitArray1[0] = {0}", bitArray1[0]);
+        Console.WriteLine("bitArray2[0] = {0}", bitArray2[0]);
+    }
+}
