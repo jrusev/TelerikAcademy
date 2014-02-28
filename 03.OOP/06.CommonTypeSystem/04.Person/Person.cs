a6dae727ff6b6d61ed3cb6cffc252e2aeca09f2b
neutrino <jrusev@gmail.com>
2014-02-28 13:27:38 +0200

    Task4 solved'

---
+++ 03.OOP/06.CommonTypeSystem/04.Person/Person.cs
@@ -0,0 +1,20 @@
+﻿// Create a class Person with two fields – name and age.
+// Age can be left unspecified (may contain null value.
+// Override ToString() to display the information of a person and if age is not specified – to say so.
+// Write a program to test this functionality.
+public class Person
+{
+    private string name;
+    private byte? age;
+
+    public Person(string name, byte? age = null)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public override string ToString()
+    {
+        return this.name + string.Format(this.age == null ? " (age not specified)" : " (age : {0})", this.age);
+    }
+}
---
+++ 03.OOP/06.CommonTypeSystem/04.Person/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    // Create a class Person with two fields – name and age.
+    // Age can be left unspecified (may contain null value.
+    // Override ToString() to display the information of a person and if age is not specified – to say so.
+    // Write a program to test this functionality.
+    internal static void Main()
+    {
+        Person p1 = new Person("Johny");
+        Person p2 = new Person("Tonny", null);
+        Person p3 = new Person("Billy", 23);
+
+        Console.WriteLine(p1);
+        Console.WriteLine(p2);
+        Console.WriteLine(p3);
+    }
+}
