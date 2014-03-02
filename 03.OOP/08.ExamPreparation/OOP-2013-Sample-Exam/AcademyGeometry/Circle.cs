189332562259b25ef7aa6096630599abf0b49bf4
neutrino <jrusev@gmail.com>
2014-03-02 13:06:32 +0200

    added geometry API

---
+++ 03.OOP/08.ExamPreparation/OOP-2013-Sample-Exam/AcademyGeometry/Circle.cs
@@ -0,0 +1,31 @@
+﻿namespace GeometryAPI
+{
+    using System;
+
+    public class Circle : Figure, IAreaMeasurable, IFlat
+    {
+        public Circle(Vector3D center, double radius)
+            : base(center)
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius { get; private set; }
+
+        public override double GetPrimaryMeasure()
+        {
+            return this.GetArea();
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * this.Radius * this.Radius;
+        }
+
+        public Vector3D GetNormal()
+        {
+            // Circles will always lay in the XY plane
+            return new Vector3D(0, 0, 1);
+        }
+    }
+}
---
+++ 03.OOP/08.ExamPreparation/OOP-2013-Sample-Exam/AcademyGeometry/Cylinder.cs
@@ -0,0 +1,45 @@
+﻿namespace GeometryAPI
+{
+    using System;
+
+    public class Cylinder : Figure, IVolumeMeasurable, IAreaMeasurable
+    {
+        // (bottomX,bottomY,bottomZ) (topX, topY, topZ) radius 
+        public Cylinder(Vector3D bottom, Vector3D top, double radius)
+            : base(bottom, top)
+        {
+            this.Bottom = bottom;
+            this.Top = top;
+            this.Radius = radius;
+        }
+
+        public Vector3D Bottom { get; private set; }
+
+        public Vector3D Top { get; private set; }
+
+        public double Radius { get; private set; }
+
+        public double Height
+        { 
+            get
+            {
+                return this.Top.Z - this.Bottom.Z;
+            }            
+        }
+
+        public override double GetPrimaryMeasure()
+        {
+            return this.GetVolume();
+        }
+
+        public double GetVolume()
+        {
+            return Math.PI * this.Radius * this.Radius * this.Height;
+        }
+
+        public double GetArea()
+        {
+            return 2 * Math.PI * this.Radius * this.Height;
+        }
+    }
+}
