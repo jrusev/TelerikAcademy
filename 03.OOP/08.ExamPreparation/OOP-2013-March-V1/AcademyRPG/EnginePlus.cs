8f086fb9554df6b6d9eb878043eb4c0926096a11
neutrino <jrusev@gmail.com>
2014-03-03 12:22:59 +0200

    added Academy RPG

---
+++ 03.OOP/08.ExamPreparation/OOP-2013-March-V1/AcademyRPG/EnginePlus.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AcademyRPG
+{
+    public class EnginePlus : Engine
+    {
+        public EnginePlus()
+            : base()
+        {
+        }
+
+        public override void ExecuteCreateObjectCommand(string[] commandWords)
+        {
+            switch (commandWords[1])
+            {
+                case "knight":
+                    {
+                        string name = commandWords[2];
+                        Point position = Point.Parse(commandWords[3]);
+                        int owner = int.Parse(commandWords[4]);
+                        this.AddObject(new Knight(name, position, owner));
+                        break;
+                    }
+                //case "guard":
+                //    {
+                //        string name = commandWords[2];
+                //        Point position = Point.Parse(commandWords[3]);
+                //        int owner = int.Parse(commandWords[4]);
+                //        this.AddObject(new Guard(name, position, owner));
+                //        break;
+                //    }
+                //case "tree":
+                //    {
+                //        int size = int.Parse(commandWords[2]);
+                //        Point position = Point.Parse(commandWords[3]);
+                //        this.AddObject(new Tree(size, position));
+                //        break;
+                //    }
+                default:
+                    base.ExecuteCreateObjectCommand(commandWords);
+                    break;
+            }            
+        }
+    }
+}
---
+++ 03.OOP/08.ExamPreparation/OOP-2013-March-V1/AcademyRPG/Knight.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AcademyRPG
+{
+    public class Knight : Character, IFighter
+    {
+        public Knight(string name, Point position, int owner)
+            : base(name, position, owner)
+        {
+            this.HitPoints = 100;
+            this.AttackPoints = 100;
+            this.DefensePoints = 100;
+        }
+
+        public int AttackPoints { get; private set; }
+
+        public int DefensePoints { get; private set; }
+
+        // Knight should always pick the first available target to attack,
+        // which is not neutral and does not belong to the same player as the Knight.
+        public int GetTargetIndex(List<WorldObject> availableTargets)
+        {
+            for (int i = 0; i < availableTargets.Count; i++)
+            {
+                if (availableTargets[i].Owner != this.Owner && availableTargets[i].Owner != 0)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
