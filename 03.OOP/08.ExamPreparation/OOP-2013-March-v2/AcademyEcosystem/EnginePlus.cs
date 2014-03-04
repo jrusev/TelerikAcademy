94249ff6c942d1fa3a5f0aea6e92fdc0b5d17b45
neutrino <jrusev@gmail.com>
2014-03-04 11:44:19 +0200

    added Ecosystem

---
+++ 03.OOP/08.ExamPreparation/OOP-2013-March-v2/AcademyEcosystem/EnginePlus.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AcademyEcosystem
+{
+    public class EnginePlus : Engine
+    {
+        protected override void ExecuteBirthCommand(string[] commandWords)
+        {
+            switch (commandWords[1])
+            {
+                case "wolf":
+                    {
+                        // birth wolf <name> <position>
+                        string name = commandWords[2];
+                        Point position = Point.Parse(commandWords[3]);
+                        this.AddOrganism(new Wolf(name, position));
+                        break;
+                    }
+                case "boar":
+                    {
+                        // birth boar <name> <position>
+                        string name = commandWords[2];
+                        Point position = Point.Parse(commandWords[3]);
+                        this.AddOrganism(new Boar(name, position));
+                        break;
+                    }
+                //case "tree":
+                //    {
+                //        Point position = Point.Parse(commandWords[2]);
+                //        this.AddOrganism(new Tree(position));
+                //        break;
+                //    }
+                //case "bush":
+                //    {
+                //        Point position = Point.Parse(commandWords[2]);
+                //        this.AddOrganism(new Bush(position));
+                //        break;
+                //    }
+                default:
+                    base.ExecuteBirthCommand(commandWords);
+                    break;
+            }            
+        }
+    }
+}
