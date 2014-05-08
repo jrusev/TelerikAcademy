d290ecd34a5397009d18d4ef46c9f746e25ead5b
neutrino <jrusev@gmail.com>
2014-05-08 21:23:45 +0300

    added HQC CodeFormatting

---
+++ Programming/04.HighQualityCode/02.Code-Formatting-Homework/CodeFormatting/01.Events/EventHolder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using Wintellect.PowerCollections;
+
+public class EventHolder
+{
+    private MultiDictionary<string, Event> dictByTitle;
+    private OrderedBag<Event> orderedByDate;
+
+    public EventHolder()
+    {
+        this.dictByTitle = new MultiDictionary<string, Event>(true);
+        this.orderedByDate = new OrderedBag<Event>();
+        this.Output = new StringBuilder();
+    }
+
+    public StringBuilder Output { get; private set; }
+
+    public void AddEvent(DateTime date, string title, string location)
+    {
+        Event newEvent = new Event(date, title, location);
+        this.dictByTitle.Add(title.ToLower(), newEvent);
+        this.orderedByDate.Add(newEvent);
+        Messages.EventAdded(this.Output);
+    }
+
+    public void DeleteEvents(string titleToDelete)
+    {
+        string title = titleToDelete.ToLower();
+        int removed = 0;
+        foreach (var eventToRemove in this.dictByTitle[title])
+        {
+            removed++;
+            this.orderedByDate.Remove(eventToRemove);
+        }
+
+        this.dictByTitle.Remove(title);
+        Messages.EventDeleted(removed, this.Output);
+    }
+
+    public void ListEvents(DateTime date, int count)
+    {
+        var eventsToShow = this.orderedByDate.RangeFrom(new Event(date, string.Empty, string.Empty), true);
+        int showed = 0;
+        foreach (var eventToShow in eventsToShow)
+        {
+            if (showed == count)
+            {
+                break;
+            }
+
+            Messages.PrintEvent(eventToShow, this.Output);
+            showed++;
+        }
+
+        if (showed == 0)
+        {
+            Messages.NoEventsFound(this.Output);
+        }
+    }
+}
---
+++ Programming/04.HighQualityCode/02.Code-Formatting-Homework/CodeFormatting/01.Events/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wintellect.PowerCollections;
+
+public class Program
+{
+    private static EventHolder events = new EventHolder();
+
+    private static void Main()
+    {
+        while (ExecuteNextCommand())
+        {
+        }
+
+        Console.WriteLine(events.Output);
+    }
+
+    private static bool ExecuteNextCommand()
+    {
+        string command = Console.ReadLine();
+        switch (command[0])
+        {
+            case 'A':
+                AddEvent(command);
+                return true;
+            case 'D':
+                DeleteEvents(command);
+                return true;
+            case 'L':
+                ListEvents(command);
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private static void ListEvents(string command)
+    {
+        int pipeIndex = command.IndexOf('|');
+        DateTime date = GetDate(command, "ListEvents");
+        string countString = command.Substring(pipeIndex + 1);
+        int count = int.Parse(countString);
+        events.ListEvents(date, count);
+    }
+
+    private static void DeleteEvents(string command)
+    {
+        string title = command.Substring("DeleteEvents".Length + 1);
+        events.DeleteEvents(title);
+    }
+
+    private static void AddEvent(string command)
+    {
+        DateTime date;
+        string title, location;
+        GetParameters(command, "AddEvent", out date, out title, out location);
+        events.AddEvent(date, title, location);
+    }
+
+    private static void GetParameters(string command, string cmdType, out DateTime date, out string title, out string location)
+    {
+        date = GetDate(command, cmdType);
+        int firstPipeIndex = command.IndexOf('|');
+        int lastPipeIndex = command.LastIndexOf('|');
+        if (firstPipeIndex == lastPipeIndex)
+        {
+            title = command.Substring(firstPipeIndex + 1).Trim();
+            location = string.Empty;
+        }
+        else
+        {
+            title = command.Substring(firstPipeIndex + 1, lastPipeIndex - firstPipeIndex - 1).Trim();
+            location = command.Substring(lastPipeIndex + 1).Trim();
+        }
+    }
+
+    private static DateTime GetDate(string command, string commandType)
+    {
+        var date = DateTime.Parse(command.Substring(commandType.Length + 1, 20));
+        return date;
+    }
+}
