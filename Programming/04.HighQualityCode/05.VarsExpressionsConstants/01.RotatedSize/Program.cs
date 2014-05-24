3d48ffbe8a5995877e4515d22fe50c38d7b0ee54
neutrino <jrusev@gmail.com>
2014-05-24 16:58:57 +0300

    added HQC 05 task 1

---
+++ Programming/04.HighQualityCode/05.VarsExpressionsConstants/01.RotatedSize/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        var size = new Size(3, 4);        
+        double angleDegr = 90;
+        double angleRads = angleDegr * Math.PI / 180;
+        var rotated = RotateSize(size, angleRads);
+
+        Console.WriteLine(size);
+        Console.WriteLine("Rotate by {0} degrees:", angleDegr);
+        Console.WriteLine(rotated);
+    }
+
+    public static Size RotateSize(Size size, double rotationRads)
+    {
+        double width = Math.Abs(Math.Cos(rotationRads) * size.Width) + Math.Abs(Math.Sin(rotationRads) * size.Height);
+        double height = Math.Abs(Math.Sin(rotationRads) * size.Width) + Math.Abs(Math.Cos(rotationRads) * size.Height);
+        var rotated = new Size(width, height);
+        return rotated;
+    }
+}
---
+++ Programming/04.HighQualityCode/05.VarsExpressionsConstants/01.RotatedSize/Size.cs
@@ -0,0 +1,17 @@
+﻿public class Size
+{
+    public Size(double width, double height)
+    {
+        this.Width = width;
+        this.Height = height;
+    }
+
+    public double Width { get; set; }
+
+    public double Height { get; set; }
+
+    public override string ToString()
+    {
+        return string.Format("width: {0:F2}, height {1:F2}", this.Width, this.Height);
+    }
+}
