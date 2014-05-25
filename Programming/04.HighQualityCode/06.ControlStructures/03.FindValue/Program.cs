cc46340421a005490850ec554c5ba6eca410c991
neutrino <jrusev@gmail.com>
2014-05-25 15:00:35 +0300

    solved HQC Control Flow

---
+++ Programming/04.HighQualityCode/06.ControlStructures/03.FindValue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] array = new int[100];
+        int expectedValue = 0;
+
+        bool isFound = FindValue(array, expectedValue);
+
+        // More code here
+        if (isFound)
+        {
+            Console.WriteLine("Value Found");
+        }
+    }
+
+    private static bool FindValue(int[] array, int expectedValue)
+    {
+        bool isFound = false;
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+            if (i % 10 == 0 && array[i] == expectedValue)
+            {
+                isFound = true;
+            }
+        }
+
+        return isFound;
+    }
+}
