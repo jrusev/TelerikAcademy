e7183f50e507f1c030fa155cfe0f58f1f2a87a99
neutrino <jrusev@gmail.com>
2014-05-25 15:04:53 +0300

    solved HQC Control Flow

--- Programming/04.HighQualityCode/06.ControlStructures/03.FindValue/Program.cs
+++ Programming/04.HighQualityCode/06.ControlStructures/03.FindValue/Program.cs
@@ -5,9 +5,9 @@ public class Program
     public static void Main()
     {
         int[] array = new int[100];
-        int expectedValue = 0;
+        int searchValue = 0;
 
-        bool isFound = FindValue(array, expectedValue);
+        bool isFound = ContainsValue(array, searchValue);
 
         // More code here
         if (isFound)
@@ -16,15 +16,16 @@ public class Program
         }
     }
 
-    private static bool FindValue(int[] array, int expectedValue)
+    private static bool ContainsValue(int[] array, int searchValue)
     {
         bool isFound = false;
         for (int i = 0; i < array.Length; i++)
         {
             Console.WriteLine(array[i]);
-            if (i % 10 == 0 && array[i] == expectedValue)
+            if (i % 10 == 0 && array[i] == searchValue)
             {
                 isFound = true;
+                break;
             }
         }
 
