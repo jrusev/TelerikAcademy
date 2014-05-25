0d753084935aac7eea8902ec1086a6f21f0f81e3
neutrino <jrusev@gmail.com>
2014-05-25 14:46:16 +0300

    add HQC Control Flow task 2

---
+++ Programming/04.HighQualityCode/06.ControlStructures/02.ConditionalStatements/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Program
+{
+    // Refactor the following if statements: 
+    //      Potato potato;
+    //      //... 
+    //      if (potato != null)
+    //         if(!potato.HasNotBeenPeeled && !potato.IsRotten)
+    //          Cook(potato);
+    // //////////////////////////
+    //      if (x >= MIN_X && (x =< MAX_X && ((MAX_Y >= y && MIN_Y <= y) && !shouldNotVisitCell)))
+    //      {
+    //         VisitCell();
+    //      }
+    public static void Main()
+    {
+    }
+
+    private static void PreparePotato()
+    {
+        Potato potato = new Potato();
+
+        // ... 
+        if (potato != null && potato.IsPeeled && potato.IsFresh)
+        {
+            // Cook(potato);
+        }
+        else
+        {
+            throw new ApplicationException("Cannot cook potato!");
+        }
+    }
+
+    private static void CheckCell(int row, int col, int[] matrix, bool isVisited)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        if (row >= 0 && row < rows && col >= 0 && col < cols && !isVisited)
+        {
+            // VisitCell();
+        }
+    }
+
+    private class Potato
+    {
+        public bool IsPeeled { get; set; }
+
+        public bool IsFresh { get; set; }
+    }
+}
