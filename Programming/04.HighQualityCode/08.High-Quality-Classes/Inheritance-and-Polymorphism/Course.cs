24a4575976657d6b3a1e1bcb2415d57e50fefd5c
neutrino <jrusev@gmail.com>
2014-05-29 16:40:20 +0300

    added HCQ 08.03

---
+++ Programming/04.HighQualityCode/08.High-Quality-Classes/Inheritance-and-Polymorphism/Course.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceAndPolymorphism
+{
+    public abstract class Course
+    {
+        public string Name { get; private set; }
+        public string TeacherName { get; private set; }
+        public IList<string> Students { get; private set; }
+
+        public Course(string courseName, string teacherName, IList<string> students)
+        {
+            this.Name = courseName;
+            this.TeacherName = teacherName;
+            this.Students = students == null ? new List<string>() : students;
+            
+        }
+
+        private string GetStudentsAsString()
+        {
+            if (this.Students == null || this.Students.Count == 0)
+            {
+                return "{ }";
+            }
+            else
+            {
+                return "{ " + string.Join(", ", this.Students) + " }";
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append("LocalCourse { Name = ");
+            result.Append(this.Name);
+            if (this.TeacherName != null)
+            {
+                result.Append("; Teacher = ");
+                result.Append(this.TeacherName);
+            }
+            result.Append("; Students = ");
+            result.Append(this.GetStudentsAsString());
+            result.Append(" }");
+            return result.ToString();
+        }
+    }
+}
