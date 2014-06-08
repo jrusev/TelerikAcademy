0be6b1f5adbf7fe8b53df2b07d357aa215c4aee7
neutrino <jrusev@gmail.com>
2014-06-08 23:29:40 +0300

    added Performance t3

--- Programming/04.HighQualityCode/10.CodeTuningOptimization/01.SolarSystem/OrbitsCalculator.cs
+++ Programming/04.HighQualityCode/10.CodeTuningOptimization/01.SolarSystem/OrbitsCalculator.cs
@@ -93,7 +93,7 @@ namespace SolarSystem
 
         private void EarthRotation()
         {
-            //for (decimal step = 0; step <= 360; step+=0.00005m)
+            //for (decimal step = 0; step <= 360; step += 0.00005m)
             //{
             //    EarthRotationAngle = ((double)step) * Days / EarthRotationPeriod;
             //}
