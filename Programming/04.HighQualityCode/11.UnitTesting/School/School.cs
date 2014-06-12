a3a8cdd5c76a321de37aebfbb61ea9ff869a1978
neutrino <jrusev@gmail.com>
2014-06-12 16:21:20 +0300

    added UnitTesting classes

--- Programming/04.HighQualityCode/11.UnitTesting/School/School.cs
+++ Programming/04.HighQualityCode/11.UnitTesting/School/School.cs
@@ -3,62 +3,55 @@ using System.Collections.Generic;
 
 public class School
 {
-    private readonly Dictionary<int, Student> idToStudent = new Dictionary<int, Student>();
-    private readonly List<Student> students = new List<Student>();
-    private readonly List<Course> courses = new List<Course>();
+    private readonly List<Student> students;
+    private readonly Student[] studentsById;
+    private readonly List<Course> courses;
 
     public School()
     {
+        this.students = new List<Student>();
+        this.studentsById = new Student[Student.MaxID - Student.MinID];
+        this.courses = new List<Course>();
     }
 
-    public int StudentCount { get { return this.students.Count; } }
-
-    public int CourseCount { get { return this.courses.Count; } }
-
-    public IEnumerable<Student> Students { get { return this.students; } }
-
-    public IEnumerable<Course> Courses { get { return this.courses; } }
-
-    public Student CreateStudent(string name)
+    public IEnumerable<Student> Students
     {
-        for (int id = Student.MinID; id <= Student.MaxID; ++id)
+        get
         {
-            if (!this.idToStudent.ContainsKey(id))
+            return this.students;
+        }
+    }
+
+    public IEnumerable<Course> Courses
+    {
+        get 
+        { 
+            return this.courses;
+        }
+    }
+
+    public Student RegisterStudent(string name)
+    {
+        for (int i = 0; i < this.studentsById.Length; i++)
+        {
+            if (this.studentsById[i] != null)
             {
-                return this.CreateStudentInner(name, id);
+                var id = Student.MinID + i;
+                var student = new Student(name, id);
+
+                this.studentsById[i] = student;
+                this.students.Add(student);
+                return student;
             }
         }
 
-        throw new InvalidOperationException("No valid ID numbers left unassigned!");
-    }
-
-    public Student CreateStudent(string name, int id)
-    {
-        if (this.idToStudent.ContainsKey(id))
-        {
-            throw new ArgumentException("Student id must be unique!");
-        }
-
-        return this.CreateStudentInner(name, id);
+        throw new InvalidOperationException("All available ID numbers are occupied!");
     }
 
     public Course CreateCourse(string name)
     {
-        var ret = new Course(name);
-        this.courses.Add(ret);
-        return ret;
-    }
-
-    public bool StudentExists(int id)
-    {
-        return this.idToStudent.ContainsKey(id);
-    }
-
-    private Student CreateStudentInner(string name, int id)
-    {
-        var ret = new Student(name, id);
-        this.idToStudent[id] = ret;
-        this.students.Add(ret);
-        return ret;
+        var course = new Course(name);
+        this.courses.Add(course);
+        return course;
     }
 }
