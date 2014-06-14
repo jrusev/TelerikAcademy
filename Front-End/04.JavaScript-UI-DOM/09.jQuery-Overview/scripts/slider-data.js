7faa8a6d8b9328a7395c8bbfcb7157e98e80a7b6
neutrino <jrusev@gmail.com>
2014-06-15 01:39:06 +0300

    added jQuery task1

---
+++ Front-End/04.JavaScript-UI-DOM/09.jQuery-Overview/scripts/slider-data.js
@@ -0,0 +1,27 @@
+var images = function () {          
+    
+    return [
+        { 
+            alt: "Flag of Argentina",
+            src: "http://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/200px-Flag_of_Argentina.svg.png"
+        }, {
+            alt: "Flag of Brazil",
+            src: "http://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/200px-Flag_of_Brazil.svg.png"                    
+        }, {
+            alt: "Flag of Germany",
+            src: "http://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/200px-Flag_of_Germany.svg.png"                   
+        }, {
+            alt: "Flag of Portugal",
+            src: "http://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Portugal.svg/200px-Flag_of_Portugal.svg.png"                    
+        }, {
+            alt: "Flag of France",
+            src: "http://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/200px-Flag_of_France.svg.png"                    
+        }, {
+            alt: "Flag of Italy",
+            src: "http://upload.wikimedia.org/wikipedia/en/thumb/0/03/Flag_of_Italy.svg/200px-Flag_of_Italy.svg.png"                    
+        }, {
+            alt: "Flag of Spain",
+            src: "http://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Flag_of_Spain.svg/200px-Flag_of_Spain.svg.png"                    
+        }
+    ];
+}
