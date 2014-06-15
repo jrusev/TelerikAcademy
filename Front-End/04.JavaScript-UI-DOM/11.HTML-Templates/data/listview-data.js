148ce4ae69df6b626595049c706555a3abbaa9ff
neutrino <jrusev@gmail.com>
2014-06-15 18:12:18 +0300

    added templates task3

---
+++ Front-End/04.JavaScript-UI-DOM/11.HTML-Templates/data/listview-data.js
@@ -0,0 +1,27 @@
+var books = [{
+    title: 'Javascript: The Good Parts'
+}, {
+    title: 'Secrets of the Javascript Ninja'
+}, {
+    title: 'Core HTML5 Canvas'
+}, {
+    title: 'Javascript Patterns'
+}];
+
+var students = [{
+    number: 1,
+    name: 'Petar Petrov',
+    mark: 5.5
+}, {
+    number: 2,
+    name: 'Stamat Georgiev',
+    mark: 4.7
+}, {
+    number: 3,
+    name: 'Maria Todorova',
+    mark: 6
+}, {
+    number: 4,
+    name: 'Georgi Geshov',
+    mark: 3.7
+}];
