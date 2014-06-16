bc195723b75af4e04ee5da7a9fd36acd79fef445
neutrino <jrusev@gmail.com>
2014-06-16 12:20:29 +0300

    added exam prep

--- Front-End/04.JavaScript-UI-DOM/12.Exam-preparation/task2/scripts.js
+++ Front-End/04.JavaScript-UI-DOM/12.Exam-preparation/task2/scripts.js
@@ -1,3 +1,17 @@
 $.fn.tabs = function () {
-
+    $container = $(this);
+    $container.addClass('tabs-container');    
+    
+    toggleVisibility($('.tab-item:first-of-type'));
+    
+    $('.tab-item').click(function() {
+        toggleVisibility($(this));
+    });
+    
+    function toggleVisibility($current) {
+        $('.tab-item').removeClass('current');
+        $current.addClass('current');
+        $('.tab-item-content').hide();
+        $('.current .tab-item-content').show();
+    }
 };
