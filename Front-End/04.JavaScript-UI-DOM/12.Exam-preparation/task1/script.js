dc5b3f5768c91fd06b7540dae9f22a3bd296b293
neutrino <jrusev@gmail.com>
2014-06-17 01:39:06 +0300

    added exam prep

--- Front-End/04.JavaScript-UI-DOM/12.Exam-preparation/task1/script.js
+++ Front-End/04.JavaScript-UI-DOM/12.Exam-preparation/task1/script.js
@@ -32,50 +32,30 @@ var styles = {
 
 function createCalendar(containerId, events) {
     var container = document.querySelector(containerId);
+    var selectedBox = null;
 
     var startDate = new Date(2014, 5, 1);
     var calendar = createCalendar(startDate, 30);
     container.appendChild(calendar);
     fillTasks(calendar, events);
-    addEventListeners();
     applyStyles(calendar);
-
-    function applyStyles(calendar) {
-        applyProps(classNames.calendar);
-        applyProps(classNames.day);
-        applyProps(classNames.date);
-        applyProps(classNames.task);
-
-        function applyProps(className) {
-            var elements = document.getElementsByClassName(className);
-            for (var i = 0, len = elements.length; i < len; i++)
-                for (var prop in styles[className])
-                    elements[i].style[prop] = styles[className][prop];
-        }
-    }
-
-    function fillTasks(calendar, events) {
-        events.forEach(function (event) {
-            var date = +event.date;
-            var dayBox = calendar.children[date - 1];
-            dayBox.children[1].innerHTML = event.hour + ' - ' + event.title;
-        });
-    }
 
     function createCalendar(date, numDays) {
         var i, day;
+        var dayTemplate = createDayTemplate();
         var calendar = document.createElement('ul');
         calendar.className += ' ' + classNames.calendar;
-        day = createDay();
         for (i = 1; i <= numDays; i += 1) {
+            day = dayTemplate.cloneNode(true);
             date.setDate(i);
             day.children[0].innerHTML = date.toDateString();
-            calendar.appendChild(day.cloneNode(true));
+            addEventListeners(day);
+            calendar.appendChild(day);
         }
         return calendar;
     }
 
-    function createDay() {
+    function createDayTemplate() {
         // this function is called only once
         var day = document.createElement('li');
         day.className += ' ' + classNames.day;
@@ -92,33 +72,47 @@ function createCalendar(containerId, events) {
         return day;
     }
 
-    function addEventListeners() {
-        var i, day;
-        var days = calendar.children;
-        for (i = 0, len = days.length; i < len; i++) {
-            day = days[i];
+    function fillTasks(calendar, events) {
+        events.forEach(function (event) {
+            var date = +event.date;
+            var dayBox = calendar.children[date - 1];
+            dayBox.children[1].innerHTML = event.hour + ' - ' + event.title;
+        });
+    }
 
-            day.addEventListener('mouseover', function (evt) {
-                if (this.children[0].style.backgroundColor !== 'rgb(255, 255, 255)')
-                    this.children[0].style.backgroundColor = 'rgb(153, 153, 153)';
-            });
+    function applyStyles(calendar) {
+        applyProps(classNames.calendar);
+        applyProps(classNames.day);
+        applyProps(classNames.date);
+        applyProps(classNames.task);
 
-            day.addEventListener('mouseout', function (evt) {
-                if (this.children[0].style.backgroundColor !== 'rgb(255, 255, 255)')
-                    this.children[0].style.backgroundColor = 'rgb(204, 204, 204)';
-            });
-
-            day.addEventListener('click', function (evt) {
-                var elements = document.getElementsByClassName(classNames.date);
-                var currentColor = this.children[0].style.backgroundColor;
-                for (var i = 0, len = elements.length; i < len; i++)
-                    elements[i].style.backgroundColor = styles[classNames.date]['backgroundColor'];
-
-                if (currentColor === 'rgb(255, 255, 255)')
-                    this.children[0].style.backgroundColor = 'rgb(204, 204, 204)';
-                else
-                    this.children[0].style.backgroundColor = 'rgb(255, 255, 255)';
-            });
+        function applyProps(className) {
+            var elements = document.getElementsByClassName(className);
+            for (var i = 0, len = elements.length; i < len; i++)
+                for (var prop in styles[className])
+                    elements[i].style[prop] = styles[className][prop];
         }
+    }
+
+    function addEventListeners(day) {
+        day.addEventListener('mouseover', function (evt) {
+            if (selectedBox !== this)
+                this.children[0].style.backgroundColor = 'rgb(153, 153, 153)';
+        });
+        day.addEventListener('mouseout', function (evt) {
+            if (selectedBox !== this)
+                this.children[0].style.backgroundColor = 'rgb(204, 204, 204)';
+        });
+        day.addEventListener('click', function (evt) {
+            if (selectedBox === this) {
+                selectedBox = null;
+                this.children[0].style.backgroundColor = 'rgb(153, 153, 153)';
+            } else {
+                if (selectedBox)
+                    selectedBox.children[0].style.background = 'rgb(204, 204, 204)';
+                this.children[0].style.backgroundColor = 'rgb(255, 255, 255)';
+                selectedBox = this;
+            }
+        });
     }
 }
