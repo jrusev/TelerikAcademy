d35024d570e7d9819ab629a3eb2d50156119691d
neutrino <jrusev@gmail.com>
2014-06-29 11:47:41 +0300

    added T4

---
+++ Programming/04.HighQualityCode/14.DevelopmentTools/Log4NetTest/Log4NetTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+
+class Program
+{
+    static void Main()
+    {
+        BasicConfigurator.Configure();
+
+        ILog log = LogManager.GetLogger("log");
+
+        log.Debug("Debug message successfulyl logged!");
+        log.Error("Error message successfulyl logged!");
+    }
+}
