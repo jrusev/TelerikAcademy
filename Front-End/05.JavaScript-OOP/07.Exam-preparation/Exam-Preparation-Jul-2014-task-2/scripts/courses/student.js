b81c24d0a6ff0e74a91fb3c18ae0d22380fc4243
neutrino <jrusev@gmail.com>
2014-07-06 11:26:45 +0300

    added exam prep

---
+++ Front-End/05.JavaScript-OOP/07.Exam-preparation/Exam-Preparation-Jul-2014-task-2/scripts/courses/student.js
@@ -0,0 +1,21 @@
+define([], function () {
+	var Student = (function () {
+		//name, exam, homework, attendance, teamwork, bonus
+		function Student(params) {
+			this.name = params.name;
+			this.exam = params.exam;
+			this.homework = params.homework;
+			this.attendance = params.attendance;
+			this.teamwork = params.teamwork;
+			this.bonus = params.bonus;
+		}
+
+		Student .prototype.toString = function () {
+			return '{' + this.name + ', exam: ' + this.exam.toFixed(2) + ', total score: '+ (this.totalScore.toFixed(2) || 'n.a.') + '}';
+		};
+
+		return Student;
+	}());
+
+	return Student;
+});
