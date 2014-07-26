1cfd2d0ac0e46d115cf343e2a8c102ce3ae6832d
neutrino <jrusev@gmail.com>
2014-07-27 00:09:36 +0300

    added AJAX

--- Front-End/06.JavaScript-Applications/06.Consuming-Remote-Data/03.Students_REST_API/scripts/JSONrequest.js
+++ Front-End/06.JavaScript-Applications/06.Consuming-Remote-Data/03.Students_REST_API/scripts/JSONrequest.js
@@ -1,12 +1,8 @@
 define(['jquery'], function ($) {
     'use strict';
 
-    // Create a module that exposes methods for performing HTTP requests by given URL and headers
-    // getJSON and postJSON
-    // Both methods should work with promises
     return {
         getJSON: function (url, callback, headers) {
-            // jQuery.ajax() returns a promise
             return $.ajax({
                 url: url,
                 headers: headers,
@@ -16,7 +12,6 @@ define(['jquery'], function ($) {
             });
         },
         postJSON: function (url, data, callback, headers) {
-            // jQuery.ajax() returns a promise
             return $.ajax({
                 url: url,
                 type: "post",
