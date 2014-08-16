6edad03c8535721aee3af375de699abc3de73673
neutrino <jrusev@gmail.com>
2014-08-17 00:06:29 +0300

    added graph demos

---
+++ Programming/05.Data-Structures-and-Algorithms/11.Graphs-Demos/Graphs-Basics/AdvancedOOP/Edge.cs
@@ -0,0 +1,20 @@
+﻿namespace GraphOOP
+{
+    internal class Edge
+    {
+        internal Edge(Node target, double distance)
+        {
+            this.Target = target;
+            this.Distance = distance;
+        }
+
+        internal Node Target { get; private set; }
+
+        internal double Distance { get; private set; }
+
+        public override string ToString()
+        {
+            return this.Target + "-" + this.Distance;
+        }
+    }
+}
---
+++ Programming/05.Data-Structures-and-Algorithms/11.Graphs-Demos/Graphs-Basics/SimpleOOP/Graph.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Graph : IEnumerable<List<int>>
+{
+    public Graph(List<int>[] successorList)
+    {
+        this.SuccessorList = successorList;
+    }
+
+    public List<int>[] SuccessorList { get; private set; }
+
+    public IEnumerator<List<int>> GetEnumerator()
+    {
+        foreach (var successors in this.SuccessorList)
+        {
+            yield return successors;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
