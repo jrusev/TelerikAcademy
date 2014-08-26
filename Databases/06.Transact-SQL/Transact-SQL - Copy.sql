e5249ccde2dcebfb78bf7a03026956250044b013
neutrino <jrusev@gmail.com>
2014-08-26 21:48:44 +0300

    added T-SQL

--- Databases/06.Transact-SQL/Transact-SQL - Copy.sql
+++ Databases/06.Transact-SQL/Transact-SQL - Copy.sql
@@ -6,42 +6,42 @@ USE TelerikAcademy
 
 -- Create table Persons
 CREATE TABLE Persons(
-    PersonID int IDENTITY PRIMARY KEY,
-    FirstName nvarchar(50) NOT NULL,
-    LastName nvarchar(50) NOT NULL,
-    SSN int NOT NULL UNIQUE CHECK (SSN >= 100000000 AND SSN <= 999999999) -- SSN is 9-digit
+	PersonID int IDENTITY PRIMARY KEY,
+	FirstName nvarchar(50) NOT NULL,
+	LastName nvarchar(50) NOT NULL,
+	SSN int NOT NULL UNIQUE CHECK (SSN >= 100000000 AND SSN <= 999999999) -- SSN is 9-digit
 )
 GO
 
 -- Create table Accounts
 CREATE TABLE Accounts(
-    AccountID int IDENTITY PRIMARY KEY,
-    PersonID int NOT NULL FOREIGN KEY REFERENCES Persons(PersonID),
-    Balance money NOT NULL
+	AccountID int IDENTITY PRIMARY KEY,
+	PersonID int NOT NULL FOREIGN KEY REFERENCES Persons(PersonID),
+	Balance money NOT NULL
 )
 GO
 
 -- Insert few records
 INSERT INTO Persons(FirstName, LastName, SSN)
 VALUES
-    ('Svetlin', 'Nakov', 100000001),
-    ('Doncho', 'Minkov', 100000002),
-    ('Nikolay', 'Kostov', 100000003),
-    ('Ivaylo', 'Kenov', 100000004)
+	('Svetlin', 'Nakov', 100000001),
+	('Doncho', 'Minkov', 100000002),
+	('Nikolay', 'Kostov', 100000003),
+	('Ivaylo', 'Kenov', 100000004)
 
 INSERT INTO Accounts(PersonID, Balance)
 VALUES
-    (1, 0),
-    (2, 5000),
-    (4, 10000),
-    (3, 20000)
+	(1, 0),
+	(2, 5000),
+	(4, 10000),
+	(3, 20000)
 GO
 
 -- Create procedure
 CREATE PROC usp_SelectPersonsFullName
 AS
-    SELECT FirstName + ' ' + LastName AS [Full Name]
-    FROM Persons
+	SELECT FirstName + ' ' + LastName AS [Full Name]
+	FROM Persons
 GO
 
 -- Test procedure
@@ -52,10 +52,10 @@ GO
 -- and returns all persons who have more money in their accounts than the supplied number.
 CREATE PROC usp_FindBalanceOver(@minBalance money)
 AS
-    SELECT *
-    FROM Persons p 
-        JOIN Accounts a ON a.PersonID = p.PersonID
-    WHERE Balance > @minBalance
+	SELECT *
+	FROM Persons p 
+		JOIN Accounts a ON a.PersonID = p.PersonID
+	WHERE Balance > @minBalance
 GO
 
 -- Test procedure
@@ -66,11 +66,11 @@ GO
 -- 3. Create a function that accepts as parameters – sum, yearly interest rate and number of months.
 -- It should calculate and return the new sum. Write a SELECT to test whether the function works as expected.
 CREATE FUNCTION ufn_BalanceWithInterest(@sum money, @yearlyRate float, @months int)
-    RETURNS money
+	RETURNS money
 AS
-    BEGIN
-        RETURN @sum * (1 + ((@yearlyRate / 100.0) / 12.0) * @months)
-    END
+	BEGIN
+		RETURN @sum * (1 + ((@yearlyRate / 100.0) / 12.0) * @months)
+	END
 GO
 
 -- Test function
@@ -84,9 +84,9 @@ GO
 -- It should take the AccountId and the interest rate as parameters.
 CREATE PROCEDURE usp_AddInterestForOneMonth(@accountID int, @yearlyRate float)
 AS
-    UPDATE Accounts
-    SET Balance = dbo.ufn_BalanceWithInterest(Balance, @yearlyRate, 1)
-    WHERE AccountID = @accountID    
+	UPDATE Accounts
+	SET Balance = dbo.ufn_BalanceWithInterest(Balance, @yearlyRate, 1)
+	WHERE AccountID = @accountID	
 GO
 
 -- Test function
@@ -100,46 +100,46 @@ GO
 -- WithdrawMoney(AccountId, money) and DepositMoney (AccountId, money) that operate in transactions.
 CREATE PROCEDURE usp_WithdrawMoney(@accountID int, @money money)
 AS
-    DECLARE @currentBalance money;
-    SELECT @currentBalance = Balance FROM Accounts
-        WHERE AccountID = @accountID
+	DECLARE @currentBalance money;
+	SELECT @currentBalance = Balance FROM Accounts
+		WHERE AccountID = @accountID
 
-    BEGIN TRAN
-        UPDATE Accounts
-        SET Balance -= @money
-        WHERE AccountID = @accountID
+	BEGIN TRAN
+		UPDATE Accounts
+		SET Balance -= @money
+		WHERE AccountID = @accountID
 
-        IF (@money < 0)
-            BEGIN
-                RAISERROR('Cannot withdraw negative amount!', 16, 1);
-                ROLLBACK TRAN;
-                RETURN;
-            END
-        ELSE IF (@currentBalance < @money)
-            BEGIN
-                RAISERROR('Insufficient balance!', 16, 1);
-                ROLLBACK TRAN;
-                RETURN;
-            END
-        ELSE
-            COMMIT TRAN
+		IF (@money < 0)
+			BEGIN
+				RAISERROR('Cannot withdraw negative amount!', 16, 1);
+				ROLLBACK TRAN;
+				RETURN;
+			END
+		ELSE IF (@currentBalance < @money)
+			BEGIN
+				RAISERROR('Insufficient balance!', 16, 1);
+				ROLLBACK TRAN;
+				RETURN;
+			END
+		ELSE
+			COMMIT TRAN
 GO
 
 CREATE PROCEDURE usp_DepositMoney(@accountID int, @money money)
 AS
-    BEGIN TRAN
-        UPDATE Accounts
-        SET Balance += @money
-        WHERE AccountID = @accountID
+	BEGIN TRAN
+		UPDATE Accounts
+		SET Balance += @money
+		WHERE AccountID = @accountID
 
-        IF (@money < 0)
-            BEGIN
-                RAISERROR('Cannot deposit negative amount!', 16, 1);
-                ROLLBACK TRAN;
-                RETURN;
-            END
-        ELSE
-            COMMIT TRAN
+		IF (@money < 0)
+			BEGIN
+				RAISERROR('Cannot deposit negative amount!', 16, 1);
+				ROLLBACK TRAN;
+				RETURN;
+			END
+		ELSE
+			COMMIT TRAN
 GO
 
 -- Test procedures
@@ -165,10 +165,10 @@ GO
 
 CREATE TRIGGER tr_OnBalanceUpdate ON Accounts FOR UPDATE
 AS
-    INSERT INTO Logs(AccountID, OldSum, NewSum)
-    SELECT del.AccountID, del.Balance, ins.Balance
-    FROM Deleted del
-        JOIN Inserted ins ON del.AccountID = ins.AccountID
+	INSERT INTO Logs(AccountID, OldSum, NewSum)
+	SELECT del.AccountID, del.Balance, ins.Balance
+	FROM Deleted del
+		JOIN Inserted ins ON del.AccountID = ins.AccountID
 GO
 
 EXEC usp_DepositMoney 1, 100
@@ -183,32 +183,32 @@ CREATE FUNCTION dbo.ufn_ContainsAllLetters(@Word nvarchar(50),  @Letters nvarcha
     RETURNS bit
 AS
 BEGIN
-    DECLARE @Contains bit = 1
-    DECLARE @Counter INT = 1
+	DECLARE @Contains bit = 1
+	DECLARE @Counter INT = 1
 
-    WHILE(@counter <= LEN(@Word))
-        BEGIN
-        IF (CHARINDEX(SUBSTRING(@Word, @Counter, 1), @Letters) = 0)
-            SET @Contains = 0
-        SET @Counter = @Counter + 1
-        END
-    RETURN @Contains
+	WHILE(@counter <= LEN(@Word))
+		BEGIN
+		IF (CHARINDEX(SUBSTRING(@Word, @Counter, 1), @Letters) = 0)
+			SET @Contains = 0
+		SET @Counter = @Counter + 1
+		END
+	RETURN @Contains
 END
 GO
 
 CREATE PROC dbo.usp_FindAllNamesWithLetters(@Letters nvarchar(50))
 AS
     DECLARE @Valid bit = 0
-    SELECT * FROM (
-        SELECT FirstName FROM Employees
-        UNION ALL
-        SELECT LastName FROM Employees
-        UNION ALL
-        SELECT Name FROM Towns
-        ) AS Temp(Name)
+	SELECT * FROM (
+		SELECT FirstName FROM Employees
+		UNION ALL
+		SELECT LastName FROM Employees
+		UNION ALL
+		SELECT Name FROM Towns
+		) AS Temp(Name)
 
-    WHERE
-        1 = (SELECT dbo.ufn_ContainsAllLetters(Name, @Letters))
+	WHERE
+		1 = (SELECT dbo.ufn_ContainsAllLetters(Name, @Letters))
 GO
 
 EXEC dbo.usp_FindAllNamesWithLetters 'oistmiahf'
@@ -232,16 +232,16 @@ DECLARE empCursor CURSOR READ_ONLY FOR
     DECLARE @e2lname nvarchar(50)
     DECLARE @town nvarchar(50)
 
-    -- Get all pairs of employees in one town
+	-- Get all pairs of employees in one town
     FETCH NEXT FROM empCursor
         INTO @town, @e1fname, @e1lname, @e2fname, @e2lname
  
     WHILE @@FETCH_STATUS = 0
-        BEGIN
-            PRINT @town + ': ' + @e1fname + ' ' + @e1lname + ', ' + @e2fname + ' ' + @e2lname
-            FETCH NEXT FROM empCursor
-                INTO @town, @e1fname, @e1lname, @e2fname, @e2lname
-        END
+		BEGIN
+			PRINT @town + ': ' + @e1fname + ' ' + @e1lname + ', ' + @e2fname + ' ' + @e2lname
+			FETCH NEXT FROM empCursor
+				INTO @town, @e1fname, @e1lname, @e2fname, @e2lname
+		END
  
 CLOSE empCursor
 DEALLOCATE empCursor
