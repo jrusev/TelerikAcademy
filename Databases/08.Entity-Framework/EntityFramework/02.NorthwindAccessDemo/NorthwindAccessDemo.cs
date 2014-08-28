e41f8c758775c8cdc5b68d02a9cdfa971e25e635
neutrino <jrusev@gmail.com>
2014-08-28 21:36:08 +0300

    added EF

--- Databases/08.Entity-Framework/EntityFramework/02.NorthwindAccessDemo/NorthwindAccessDemo.cs
+++ Databases/08.Entity-Framework/EntityFramework/02.NorthwindAccessDemo/NorthwindAccessDemo.cs
@@ -2,15 +2,17 @@
 using Northwind.Models;
 using Northwind.DataAccess;
 
+// Create a DAO class with static methods which provide functionality for
+// inserting, modifying and deleting customers. Write a testing class.
 class NorthwindAccessDemo
 {
     public static void Main()
     {
-        Console.WriteLine("Connecting to database...");
+        Console.WriteLine("Connecting to server...");
 
-        Console.WriteLine("Insert: ({0} affected row(s))", TestInsert());
-        Console.WriteLine("Modify: ({0} affected row(s))", TestModify());
-        Console.WriteLine("Delete: ({0} affected row(s))", TestDelete());
+        Console.WriteLine("Insert: ({0} row(s) affected)", TestInsert());
+        Console.WriteLine("Modify: ({0} row(s) affected)", TestModify());
+        Console.WriteLine("Delete: ({0} row(s) affected)", TestDelete());
     }
 
     private static int TestInsert()
