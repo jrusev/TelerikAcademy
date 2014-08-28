444e23bd3ea196b5ed92fd4e103af102cc17f780
neutrino <jrusev@gmail.com>
2014-08-28 13:13:28 +0300

    added trees

--- Programming/05.DataStructuresAndAlgorithms/03.Trees-and-Traversals/01.SimpleTree/Program.cs
+++ Programming/05.DataStructuresAndAlgorithms/03.Trees-and-Traversals/01.SimpleTree/Program.cs
@@ -3,20 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-// You are given a tree of N nodes represented as a set of N-1 pairs of nodes (parent node, child node),
-// each in the range (0..N-1). Write a program to read the tree and find:
-// 
-// 1) the root node
-// 2) all leaf nodes
-// 3) all middle nodes
-// 4) the longest path in the tree
-// 5) all paths in the tree with given sum S of their nodes
-// 6) all subtrees with given sum S of their nodes
-//          ________3________
-//          |               |
-//  ________5________       2
-//  |       |       |       |
-//  0       1       6       4
 public class Program
 {
     private static Node<int>[] nodes;
