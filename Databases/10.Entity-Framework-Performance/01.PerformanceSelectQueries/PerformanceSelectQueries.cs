0b080669ce60c342094a87a6c2a6748b6eb8b5fa
neutrino <jrusev@gmail.com>
2014-09-02 02:40:48 +0300

    added trie

---
+++ Databases/10.Entity-Framework-Performance/01.PerformanceSelectQueries/PerformanceSelectQueries.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using TelerikAcademy.Model;
+using System.Diagnostics;
+
+// Using Entity Framework write a SQL query to select all employees from the Telerik Academy database
+// and later print their name, department and town. Try the both variants: with and without .Include().
+// Compare the number of executed SQL statements and the performance.
+public class PerformanceSelectQueries
+{
+    public static void Main()
+    {
+        SlowEmployeeQuery();
+        FastEmployeeQuery();
+
+        Console.WriteLine();
+
+        SlowEmployeeQuery();
+        FastEmployeeQuery();
+
+        Console.WriteLine();
+
+        SlowEmployeeQuery();
+        FastEmployeeQuery();
+    }
+
+    public static void SlowEmployeeQuery()
+    {
+        using (var context = new TelerikAcademyEntities())
+        {
+            var sw = Stopwatch.StartNew();
+            foreach (var employee in context.Employees)
+            {
+                var Name = employee.LastName;
+                var Department = employee.Department.Name;
+                var Town = employee.Address.Town.Name;
+            }
+
+            sw.Stop();
+            Console.WriteLine("Slow: {0} milliseconds.", sw.Elapsed.TotalMilliseconds);
+        }
+    }
+
+    public static void FastEmployeeQuery()
+    {
+        using (var context = new TelerikAcademyEntities())
+        {
+            var sw = Stopwatch.StartNew();
+            foreach (var employee in context.Employees.Include("Department").Include("Address.Town"))
+            {
+                var Name = employee.LastName;
+                var Department = employee.Department.Name;
+                var Town = employee.Address.Town.Name;
+            }
+
+            sw.Stop();
+            Console.WriteLine("Fast: {0} milliseconds.", sw.Elapsed.TotalMilliseconds);
+        }
+    }
+}
---
+++ Databases/10.Entity-Framework-Performance/02.PerformanceToList/PerformanceToList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using TelerikAcademy.Model;
+using System.Diagnostics;
+
+// Using Entity Framework write a query that selects all employees from the Telerik Academy database,
+// then invokes ToList(), then selects their addresses, then invokes ToList(), then selects their towns,
+// then invokes ToList() and finally checks whether the town is "Sofia".
+// Rewrite the same in more optimized way and compare the performance.
+public class PerformanceToList
+{
+    public static void Main()
+    {
+        SlowToListOperation();
+        FastToListOperation();
+
+        Console.WriteLine();
+
+        SlowToListOperation();
+        FastToListOperation();
+
+        Console.WriteLine();
+
+        SlowToListOperation();
+        FastToListOperation();
+    }
+
+    public static void SlowToListOperation()
+    {
+        using (var context = new TelerikAcademyEntities())
+        {
+            var sw = Stopwatch.StartNew();
+
+            var sofiaEmployees = context.Employees.Select(e => e).ToList().Select(e => e.Address).ToList().Select(a => a.Town).
+                ToList().Where(t => t.Name.ToLower() == "sofia");
+
+            var count = sofiaEmployees.Count();
+            sw.Stop();
+            Console.WriteLine("Slow: {0} milliseconds.", sw.Elapsed.TotalMilliseconds);
+        }
+    }
+
+    public static void FastToListOperation()
+    {
+        using (var context = new TelerikAcademyEntities())
+        {
+            var sw = Stopwatch.StartNew();
+
+            var sofiaEmployees = context.Employees.Select(e => e).Select(e => e.Address).Select(a => a.Town)
+                .Where(t => t.Name.ToLower() == "sofia");
+
+            var count = sofiaEmployees.Count();
+            sw.Stop();
+            Console.WriteLine("Slow: {0} milliseconds.", sw.Elapsed.TotalMilliseconds);
+        }
+    }
+}
