6028a281f452e403ce9c957ce0c51bfccd93d8df
neutrino <jrusev@gmail.com>
2014-09-05 00:06:24 +0300

    added mongochat

--- Databases/15.MongoDB-with-DotNET/MongoChat/MongoChat.Client/MongoChatClient.cs
+++ Databases/15.MongoDB-with-DotNET/MongoChat/MongoChat.Client/MongoChatClient.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 using MongoDB.Driver;
-using MongoChat.Models;
+using MongoDB.Bson;
+using MongoChat.Client;
 
 class MongoChatClient
 {
-    const string MongoCloud = "mongodb://telerik:chat@ds035300.mongolab.com:35300/mongochat";
-    const string MongoLocal = "mongodb://localhost";
-
     static void Main()
     {
-        string connString = MongoCloud;
-
-        var db = new MongoClient(connString).GetServer().GetDatabase("mongochat");
-        var messages = db.GetCollection<Message>("Messages");
+        var db = new MongoClient(Connections.Default.MongoCloud).GetServer().GetDatabase("mongochat");
+        var messages = db.GetCollection<BsonDocument>("Messages");
 
         Console.Write("Username: ");
         string username = Console.ReadLine();
@@ -20,7 +16,7 @@ class MongoChatClient
         while (true)
         {
             Console.Write("> ");
-            messages.Insert(new Message(username, Console.ReadLine(), DateTime.Now));
+            messages.Insert(new BsonDocument { { "Author", username }, { "Text", Console.ReadLine() }, { "Time", DateTime.Now } });
             Console.WriteLine(string.Join(Environment.NewLine, messages.FindAll()));
         }
     }
